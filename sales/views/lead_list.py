ecd166d6840da1192035dd3926f1f21ae7aee321
Phan Hong Phuc FDS <phuc.phan@fdssoft>
2022-10-19 17:44:34 +0700

    add api filters activities

--- sales/views/lead_list.py
+++ sales/views/lead_list.py
@@ -7,7 +7,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django_filters import rest_framework as filters
-
+from ..filters.lead_list import ActivitiesFilter
 
 PASS_FIELDS = ['user_create', 'user_update', 'lead']
 
@@ -128,19 +128,6 @@ class LeadDetailViewSet(viewsets.ViewSet):
         ld = get_object_or_404(queryset, pk=pk)
         ld.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class ActivitiesFilter(filters.FilterSet):
-    status = filters.MultipleChoiceFilter(choices=Activities.Status.choices)
-    tag = filters.MultipleChoiceFilter(choices=Activities.Tags.choices)
-    phase = filters.MultipleChoiceFilter(choices=Activities.Phases.choices)
-    assigned_to = filters.CharFilter(field_name='assigned_to', lookup_expr='icontains')
-    start_date = filters.DateFilter(field_name='start_date', lookup_expr='gte')
-    end_date = filters.DateFilter(field_name='end_date', lookup_expr='lte')
-    
-    class Meta:
-        model = Activities
-        fields = ('status', 'tag', 'phase', 'assigned_to', 'start_date', 'end_date')
 
 
 class LeadActivitiesViewSet(generics.ListCreateAPIView):
