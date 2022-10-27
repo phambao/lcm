dbaa0c2939445a9ed1e3835e0635c3d58e27553c
bao <bao.pham@fdssoft.com>
2022-10-27 16:52:59 +0700

    Fixing cors for dev

--- lcm/settings.py
+++ lcm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'drf_yasg',
     'django_filters',
     'knox',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,6 +139,12 @@ REST_KNOX = {
     # Never expire token
     'TOKEN_TTL': None,
 }
+
+# Build for local development
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3333",
+    "http://127.0.0.1:3333",
+]
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-DATABASE-ATOMIC_REQUESTS
 ATOMIC_REQUESTS = True
