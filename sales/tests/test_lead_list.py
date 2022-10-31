f087689b926435b22599b19881d42c503c7f6d53
Phan Hong Phuc FDS <phuc.phan@fdssoft>
2022-10-31 14:53:09 +0700

    create setupclass

--- sales/tests/test_lead_list.py
+++ sales/tests/test_lead_list.py
@@ -2,50 +2,68 @@ from rest_framework import status
 
 from .test_base import BaseTest
 from ..models import lead_list
+from base.models import country_state_city
 from api.models import User
 
 
 class LeadDetailTests(BaseTest):
 
-    def setUp(self):
-        res_contact_types = self.client.post('/api/sales/contact-types/',
-                         {'name': 'test'}, HTTP_AUTHORIZATION=self.token)
-        self.contact_type_id = res_contact_types.data['id']
-        res_project_types = self.client.post('/api/sales/lead-list/project-types/',
-                         {'name': 'test'}, HTTP_AUTHORIZATION=self.token)
-        self.project_type_id = res_project_types.data['id']
-        res_tag_activity = self.client.post('/api/sales/lead-list/activity/tags/',
-                         {'name': 'test'}, HTTP_AUTHORIZATION=self.token)
-        self.tag_activity_id = res_tag_activity.data['id']
-        res_phase_activity = self.client.post('/api/sales/lead-list/activity/phase/',
-                         {'name': 'test'}, HTTP_AUTHORIZATION=self.token)
-        self.phase_activity_id = res_phase_activity.data['id']
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.country = country_state_city.Country.objects.create(name='USA')
+        cls.state = country_state_city.State.objects.create(name='California', country=cls.country)
+        cls.city = country_state_city.City.objects.create(name='Los Angeles', state=cls.state, country=cls.country)
+        cls.tag_lead = lead_list.TagLead.objects.create(name='Test')
+        cls.contact_types = lead_list.ContactTypeName.objects.create(name='Test')
+        cls.project_types = lead_list.ProjectType.objects.create(name='Test')
+        cls.tag_activity = lead_list.TagActivity.objects.create(name='Test')
+        cls.phase_activity = lead_list.PhaseActivity.objects.create(name='Test')
 
     def test_lead_detail_create(self):
-        response = self.client.post('/api/sales/lead-list/leads/', {
-            "activities": [],
-            "contacts": [],
-            "photos": [],
-            "city": {},
-            "state": {},
-            "country": {},
-            "project_types": [
-                {
-                    "id": self.project_type_id
-                }
-            ],
-            "lead_title": "Test",
-            "salesperson": [{"id": self.user_id}],
-            "projected_sale_date": "2022-01-01T00:00:00Z",
-            "zip_code": "",
-            "status": "open",
-            "proposal_status": "approved",
-            "notes": "",
-            "confidence": 1,
-            "estimate_revenue_from": "1.00",
-            "estimate_revenue_to": "1.00",
-            "source": "",
-            "tags": []
-        }, HTTP_AUTHORIZATION=self.token, format='json')
+        response = self.client.post(
+            '/api/sales/lead-list/leads/', 
+            {
+                'lead_title': 'Test',
+                'activities': [],
+                'contacts': [],
+                'photos': [],
+                'city': {},
+                'state': {},
+                'country': {},
+                'project_types': [
+                    {
+                        'id': self.project_types.id
+                    }
+                ],
+                
+                'salesperson': [
+                    {
+                        'id': self.user_id
+                    }
+                ],
+                'projected_sale_date': '2022-01-01T00:00:00Z',
+                'zip_code': '',
+                'status': 'open',
+                'proposal_status': 'approved',
+                'notes': '',
+                'confidence': 1,
+                'estimate_revenue_from': '1.00',
+                'estimate_revenue_to': '1.00',
+                'source': '',
+                'tags': [
+                    {
+                        'id': self.tag_lead.id
+                    }
+                ]
+
+            },
+            format='json',
+            HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_lead_detail_get(self):
+        response = self.client.get(
+            '/api/sales/lead-list/leads/', HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
