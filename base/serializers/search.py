068133eb7e93bccaf65b6dc2bb006e18296f5bc2
bao <bao.pham@fdssoft.com>
2022-11-07 09:39:03 +0700

    Update Search function

--- base/serializers/search.py
+++ base/serializers/search.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from ..models.search import Search
+from django.contrib.contenttypes.models import ContentType
 
 
 class SearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Search
         fields = ('id', 'name', 'params', 'content_type', 'user')
+
+
+class ContentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentType
+        fields = ('id', 'app_label', 'model')
