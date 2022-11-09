227a1aaf9e23458ceaa240d19f155c8ddb21a643
Phan Hong Phuc FDS <phuc.phan@fdssoft>
2022-11-09 15:30:57 +0700

    config mail, reset password

--- lcm/settings.py
+++ lcm/settings.py
@@ -175,3 +175,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = "uploads/"
 MEDIA_URL = '/media/'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
