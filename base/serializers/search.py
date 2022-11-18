8f5c9174d0afc47fb3c2d4ad48ea2dd7ebd49fda
bao <bao.pham@fdssoft.com>
2022-11-18 13:48:32 +0700

    Add config api - column and search

--- base/serializers/search.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models.search import Search
-
-
-class SearchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Search
-        fields = ('id', 'name', 'params', 'content_type', 'user')
