bd7ade7dc35a9f2271086c80e9a88e7136832741
Bao <bao.pham@fdssoft.com>
2023-03-13 14:30:03 +0700

    catch the error when no table in db

--- sales/apps.py
+++ sales/apps.py
@@ -13,10 +13,14 @@ class SalesConfig(AppConfig):
     def ready(self):
         from django.contrib.contenttypes.models import ContentType
         from sales.models.estimate import POFormula, DataEntry, UnitLibrary, DescriptionLibrary
+        from psycopg2.errors import UndefinedTable
 
         global PO_FORMULA_CONTENT_TYPE, DATA_ENTRY_CONTENT_TYPE,\
             UNIT_LIBRARY_CONTENT_TYPE, DESCRIPTION_LIBRARY_CONTENT_TYPE
-        PO_FORMULA_CONTENT_TYPE = ContentType.objects.get_for_model(POFormula).pk
-        DATA_ENTRY_CONTENT_TYPE = ContentType.objects.get_for_model(DataEntry).pk
-        UNIT_LIBRARY_CONTENT_TYPE = ContentType.objects.get_for_model(UnitLibrary).pk
-        DESCRIPTION_LIBRARY_CONTENT_TYPE = ContentType.objects.get_for_model(DescriptionLibrary).pk
+        try:
+            PO_FORMULA_CONTENT_TYPE = ContentType.objects.get_for_model(POFormula).pk
+            DATA_ENTRY_CONTENT_TYPE = ContentType.objects.get_for_model(DataEntry).pk
+            UNIT_LIBRARY_CONTENT_TYPE = ContentType.objects.get_for_model(UnitLibrary).pk
+            DESCRIPTION_LIBRARY_CONTENT_TYPE = ContentType.objects.get_for_model(DescriptionLibrary).pk
+        except UndefinedTable:
+            """Ignore when no table in db, only used for testing"""
