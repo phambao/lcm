f84a62c8776738c41b84b63b2e237c50814f376e
Bao <bao.pham@fdssoft.com>
2023-03-29 17:32:56 +0700

    filter formula by group and assemble

--- sales/filters/estimate.py
+++ sales/filters/estimate.py
@@ -1,1 +1,12 @@
 from django_filters import rest_framework as filters
+
+from sales.models import POFormula, POFormulaGrouping, Assemble
+
+
+class FormulaFilter(filters.FilterSet):
+    group = filters.BooleanFilter(field_name='group', lookup_expr='isnull', exclude=True)
+    assemble = filters.BooleanFilter(field_name='assemble', lookup_expr='isnull', exclude=True)
+
+    class Meta:
+        model = POFormula
+        fields = ('group', 'assemble')
