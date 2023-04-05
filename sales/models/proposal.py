9414bf746275e4daf401b9a0c6a57f5dc647c1cc
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-04-05 13:46:01 +0700

    create model proposal, add ordering event, todo

--- sales/models/proposal.py
+++ sales/models/proposal.py
@@ -1,8 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 
 from api.models import BaseModel
-from base.models.config import Config
 
 
 class ProposalTemplate(BaseModel):
@@ -16,8 +14,8 @@ class ProposalWidget(BaseModel):
     class Meta:
         db_table = 'proposal_widget'
 
-    proposal_template = models.ForeignKey(ProposalTemplate, on_delete=models.CASCADE, related_name='proposal__template_widget')
-    type = models.CharField(max_length=64, blank=True)
+    proposal_template = models.ForeignKey(ProposalTemplate, on_delete=models.CASCADE, related_name='proposal_template_widget')
+    type_widget = models.CharField(max_length=64, blank=True)
     title = models.CharField(max_length=128, blank=True)
     display_order = models.IntegerField(blank=True, null=True)
     data_widget = models.JSONField(default=dict)
