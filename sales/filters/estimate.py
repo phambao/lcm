ef6d9648c537c96a976348ed31ba0f25926f40af
Bao <bao.pham@fdssoft.com>
2023-04-11 11:57:17 +0700

    Search and filter assemble

--- sales/filters/estimate.py
+++ sales/filters/estimate.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 
 from sales.models import POFormula, POFormulaGrouping, Assemble, EstimateTemplate
@@ -19,3 +20,14 @@ class EstimateTemplateFilter(filters.FilterSet):
     class Meta:
         model = EstimateTemplate
         fields = ('name', 'created_date', 'modified_date')
+
+
+class AssembleFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    created_date = filters.DateTimeFilter(field_name='created_date', lookup_expr='gt')
+    formula_name = filters.CharFilter(field_name='assemble_formulas__name', lookup_expr='icontains')
+    user_create = filters.ModelMultipleChoiceFilter(queryset=get_user_model().objects.all())
+
+    class Meta:
+        model = Assemble
+        fields = ('name', 'created_date', 'formula_name', 'user_create')
