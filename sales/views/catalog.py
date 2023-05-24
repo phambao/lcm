607abd63cb6da4cbd13948ee45324a667bdcd4c2
Bao <bao.pham@fdssoft.com>
2023-05-24 09:48:26 +0700

    revert code

--- sales/views/catalog.py
+++ sales/views/catalog.py
@@ -301,16 +301,19 @@ def parse_c_table(children):
     data = []
     for child in children:
         try:
-            ancestor = child.get_full_ancestor()
-            levels = [CatalogEstimateSerializer(c).data for c in ancestor[::-1]]
+            try:
+                ancestor = child.get_full_ancestor()
+                levels = [CatalogEstimateSerializer(c).data for c in ancestor[::-1]]
+            except:
+                levels = []
+            c_table = child.c_table
+            header = c_table['header']
+            for i, d in enumerate(c_table['data']):
+                content = {**{header[j]: d[j] for j in range(len(header))}, **{"id": f'{child.pk}:{i}'},
+                           'levels': levels}
+                data.append(content)
         except:
-            levels = []
-        c_table = child.c_table
-        header = c_table['header']
-        for i, d in enumerate(c_table['data']):
-            content = {**{header[j]: d[j] for j in range(len(header))}, **{"id": f'{child.pk}:{i}'},
-                       'levels': levels}
-            data.append(content)
+            """Some old data is not valid"""
     return data
 
 
