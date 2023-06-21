dce2ce4ba7fbf8d01ca77b09060e1d1c5b8b481f
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-06-21 14:25:24 +0700

    add timezone in company

--- base/serializers/config.py
+++ base/serializers/config.py
@@ -69,7 +69,7 @@ class CompanySerializer(serializers.ModelSerializer):
         model = CompanyBuilder
         fields = ('id', 'logo', 'company_name', 'address', 'city', 'state', 'zip_code', 'tax', 'size',
                   'business_phone', 'fax', 'email', 'cell_phone', 'cell_mail', 'created_date', 'modified_date',
-                  'user_create', 'user_update', 'currency', 'description')
+                  'user_create', 'user_update', 'currency', 'description', 'company_timezone')
 
     # def create(self, validated_data):
     #     request = self.context['request']
