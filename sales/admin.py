2100e08e110611d200affb02b4b7185b6653cfd3
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-06-22 14:07:43 +0700

    add schedule event, daily log, todo, update api get in schedule

--- sales/admin.py
+++ sales/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from sales.models.catalog import Catalog
 from sales.models.lead_list import LeadDetail
 from sales.models.estimate import EstimateTemplate
+from sales.models.lead_schedule import ToDo, ScheduleEvent, DailyLog
 from sales.models.proposal import ProposalWriting
 # Register your models here.
 
@@ -29,6 +30,24 @@ class ProposalWritingAdmin(FilterUserCreateAdmin):
     search_fields = ['name']
 
 
+class ScheduleTodoAdmin(FilterUserCreateAdmin):
+    list_display = ('id', 'title')
+    search_fields = ['title']
+
+
+class ScheduleEventAdmin(FilterUserCreateAdmin):
+    list_display = ('id', 'event_title')
+    search_fields = ['event_title']
+
+
+class ScheduleDailyLogAdmin(FilterUserCreateAdmin):
+    list_display = ('id', 'title')
+    search_fields = ['title']
+
+
+admin.site.register(DailyLog, ScheduleDailyLogAdmin)
+admin.site.register(ScheduleEvent, ScheduleEventAdmin)
+admin.site.register(ToDo, ScheduleTodoAdmin)
 admin.site.register(Catalog, CatalogAdmin)
 admin.site.register(LeadDetail, LeadDetailAdmin)
 admin.site.register(EstimateTemplate, EstimateTemplateAdmin)
