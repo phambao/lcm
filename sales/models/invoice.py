60e922c89aed7de66789b440801a2277f1fd0498
Bao <bao.pham@fdssoft.com>
2023-07-12 14:53:14 +0700

    update invoice api

--- sales/models/invoice.py
+++ sales/models/invoice.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -13,7 +14,17 @@ class TableInvoice(BaseModel):
     type = models.CharField(max_length=32, choices=TableTypeInvoice.choices, default=TableTypeInvoice.CHANGE_ORDER)
     change_orders = ArrayField(models.IntegerField(blank=True), default=list, blank=True)
     invoice = models.ForeignKey('sales.Invoice', blank=True, null=True, on_delete=models.CASCADE, related_name='tables')
-    # proposal_items = ArrayField(models.IntegerField(blank=True), default=list, blank=True)
+    progress_payment = ArrayField(models.JSONField(blank=True, null=True), default=list, blank=True)
+
+
+class PaymentHistory(BaseModel):
+    invoice = models.ForeignKey('sales.Invoice', blank=True, null=True,
+                                on_delete=models.CASCADE, related_name='payment_histories')
+    date = models.DateField()
+    amount = models.DecimalField(default=0, max_digits=32, decimal_places=2)
+    unit_cost = models.CharField(max_length=32)
+    received_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='payments',
+                                    null=True, blank=True)
 
 
 class Invoice(BaseModel):
--- sales/serializers/invoice.py
+++ sales/serializers/invoice.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
+from api.serializers.base import SerializerMixin
 from base.utils import pop
 from .change_order import ChangeOrderSerializer
-from api.serializers.base import SerializerMixin
 from .proposal import ProposalWritingSerializer
 from ..models import ChangeOrder
-from ..models.invoice import Invoice, TableInvoice
+from ..models.invoice import Invoice, TableInvoice, PaymentHistory
+
+
+class PaymentHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentHistory
+        fields = ('id', 'date', 'amount', 'unit_cost', 'received_by')
 
 
 class TableInvoiceSerializer(serializers.ModelSerializer, SerializerMixin):
@@ -23,11 +28,12 @@ class TableInvoiceSerializer(serializers.ModelSerializer, SerializerMixin):
 
 class InvoiceSerializer(serializers.ModelSerializer, SerializerMixin):
     tables = TableInvoiceSerializer('invoice', many=True, required=False, allow_null=True)
+    payment_histories = PaymentHistorySerializer('invoice', many=True, required=False, allow_null=True)
 
     class Meta:
         model = Invoice
         fields = ('id', 'name', 'tables', 'date_paid', 'status', 'deadline', 'deadline_date',
-                  'deadline_time', 'comment', 'note', 'proposal')
+                  'deadline_time', 'comment', 'note', 'proposal', 'payment_histories')
 
     def create_talbes(self, instance, tables):
         for table in tables:
@@ -35,17 +41,29 @@ class InvoiceSerializer(serializers.ModelSerializer, SerializerMixin):
             serializer.is_valid(raise_exception=True)
             obj = serializer.save(invoice=instance)
 
+    def create_payment_history(self, instance, payment_histories):
+        for ph in payment_histories:
+            user = pop(ph, 'received_by', None)
+            serializer = PaymentHistorySerializer(data=ph, context=self.context)
+            serializer.is_valid(raise_exception=True)
+            obj = serializer.save(invoice=instance, received_by=user)
+
     def create(self, validated_data):
         tables = pop(validated_data, 'tables', [])
+        payment_histories = pop(validated_data, 'payment_histories', [])
         instance = super().create(validated_data)
         self.create_talbes(instance, tables)
+        self.create_payment_history(instance, payment_histories)
         return instance
 
     def update(self, instance, validated_data):
         tables = pop(validated_data, 'tables', [])
+        payment_histories = pop(validated_data, 'payment_histories', [])
         instance = super().update(instance, validated_data)
         instance.tables.all().delete()
+        instance.payment_histories.all().delete()
         self.create_talbes(instance, tables)
+        self.create_payment_history(instance, payment_histories)
         return instance
 
     def to_representation(self, instance):
