e007ce94b1e1d596c0a29fe3dd89d0897a8fbd03
Bao <bao.pham@fdssoft.com>
2023-07-25 13:45:23 +0700

    Remove custom group django

--- base/serializers/auth.py
+++ base/serializers/auth.py
@@ -1,11 +1,19 @@
 from django.contrib.auth.models import Group, Permission
 from rest_framework import serializers
 
+from api.models import GroupCompany
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'name', 'permissions')
+
+    def create(self, validated_data):
+        company = self.context['request'].user.company
+        instance = super().create(validated_data)
+        GroupCompany.objects.create(company=company, group=instance)
+        return instance
 
 
 class PermissionSerializer(serializers.ModelSerializer):
