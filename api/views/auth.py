f286760a4403b366cf3b05d019f47bf3b66557f5
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-08-08 14:34:06 +0700

    api create link and send mail

--- api/views/auth.py
+++ api/views/auth.py
@@ -9,7 +9,6 @@ from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
-
 import jwt
 
 from base.models.payment import PaymentHistoryStripe
