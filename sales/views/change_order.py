642fbc874e4e370c1a541b07d1a48c17d1b71367
Bao <bao.pham@fdssoft.com>
2023-08-10 17:14:32 +0700

    delete multiple object api

--- sales/views/change_order.py
+++ sales/views/change_order.py
@@ -1,8 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
+from api.models import ActivityLog, Action
 from base.permissions import ChangeOrderPermissions
 from base.views.base import CompanyFilterMixin
 from sales.models import ChangeOrder, ProposalWriting, POFormula
@@ -41,3 +43,17 @@ def get_items(request, pk):
     data = POFormulaDataSerializer(po_formulas, context={'request': request}, many=True).data
 
     return Response(status=status.HTTP_200_OK, data=data)
+
+
+@api_view(['DELETE'])
+@permission_classes([permissions.IsAuthenticated & ChangeOrderPermissions])
+def delete_change_orders(request):
+    ids = request.data
+    objs = ChangeOrder.objects.filter(pk__in=ids)
+    for obj in objs:
+        ActivityLog.objects.create(
+            content_type=ContentType.objects.get_for_model(ChangeOrder), content_object=obj,
+            object_id=obj.pk, action=Action.DELETE, last_state=change_order.ChangeOrderSerializer(obj).data, next_state={}
+        )
+    objs.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
