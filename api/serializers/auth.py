733a028d71c90fe532928c6088826352591f87e1
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-08-14 14:59:14 +0700

    add api check code create account

--- api/serializers/auth.py
+++ api/serializers/auth.py
@@ -13,7 +13,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'last_name', 'first_name', 'image', 'company', 'user_permissions', 'groups')
+        fields = ('id', 'email', 'username', 'last_name', 'first_name', 'image', 'company', 'user_permissions', 'groups', 'is_active')
 
     def to_representation(self, instance):
         data = super(UserSerializer, self).to_representation(instance)
