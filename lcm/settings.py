d30453c1a917a44e5778368ee5a1a45b2bf2323b
Bao <bao.pham@fdssoft.com>
2023-09-26 09:24:57 +0700

    add internal_ips for docker

--- lcm/settings.py
+++ lcm/settings.py
@@ -236,8 +236,6 @@ if USE_DEBUG_TOOLBAR:
 
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.insert(2, "debug_toolbar.middleware.DebugToolbarMiddleware")
-    INTERNAL_IPS = [
-        # ...
-        "127.0.0.1",
-        # ...
-    ]
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
