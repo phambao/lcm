78f3d2b9511fa796faf2465771744168ee65d77b
Bao <bao.pham@fdssoft.com>
2023-11-02 15:35:39 +0700

    add filter change order by lead

---
+++ sales/filters/change_order.py
@@ -0,0 +1,17 @@
+from django_filters import rest_framework as filters
+
+from sales.models import ChangeOrder, LeadDetail
+
+
+class ChangeOrderFilter(filters.FilterSet):
+    lead = filters.ModelMultipleChoiceFilter(method='filter_by_leads', queryset=LeadDetail.objects.all())
+
+    class Meta:
+        model = ChangeOrder
+        fields = ('lead', )
+
+    def filter_by_leads(self, queryset, name, value):
+        print(value)
+        if value:
+            return queryset.filter(proposal_writing__lead__in=value).distinct()
+        return queryset
--- sales/views/change_order.py
+++ sales/views/change_order.py
@@ -2,24 +2,33 @@ from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework import filters as rf_filters
+from django_filters import rest_framework as filters
 
 from base.permissions import ChangeOrderPermissions
 from base.views.base import CompanyFilterMixin
 from sales.models import ChangeOrder, ProposalWriting, POFormula
 from sales.serializers import change_order
 from sales.serializers.estimate import POFormulaDataSerializer
+from sales.filters.change_order import ChangeOrderFilter
 
 
 class ChangeOderList(CompanyFilterMixin, generics.ListCreateAPIView):
     queryset = ChangeOrder.objects.all().prefetch_related('existing_estimates', 'flat_rate_groups', 'groups').order_by('-modified_date')
     serializer_class = change_order.ChangeOrderSerializer
     permission_classes = [permissions.IsAuthenticated & ChangeOrderPermissions]
+    filter_backends = (filters.DjangoFilterBackend, rf_filters.SearchFilter)
+    filterset_class = ChangeOrderFilter
+    search_fields = ('name',)
 
 
 class ChangeOderListCompact(CompanyFilterMixin, generics.ListCreateAPIView):
     queryset = ChangeOrder.objects.all().select_related('proposal_writing')
     serializer_class = change_order.ChangeOrderSerializerCompact
     permission_classes = [permissions.IsAuthenticated & ChangeOrderPermissions]
+    filter_backends = (filters.DjangoFilterBackend, rf_filters.SearchFilter)
+    filterset_class = ChangeOrderFilter
+    search_fields = ('name',)
 
 
 class ChangeOderDetail(CompanyFilterMixin, generics.RetrieveUpdateDestroyAPIView):
