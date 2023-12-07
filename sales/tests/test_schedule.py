86f676bda92a481497517a5707c34a1a2c65bed3
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2023-12-07 11:45:25 +0700

    update api export catalog, update schedule event test

--- sales/tests/test_schedule.py
+++ sales/tests/test_schedule.py
@@ -106,8 +106,6 @@ class LeadScheduleTests(BaseTest):
             schedule_daily_log_data,
             format='json',
             HTTP_AUTHORIZATION=self.token)
-        print('***************')
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.daily_log = response.data['id']
 
@@ -169,6 +167,8 @@ class LeadScheduleTests(BaseTest):
             format='json',
             HTTP_AUTHORIZATION=self.token)
 
+        print('***************')
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.event_id = response.data['id']
 
