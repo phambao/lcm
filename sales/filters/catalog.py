687db52e9c2bbf0ca98229fd7e439d6c03ce07aa
Bao <bao.pham@fdssoft.com>
2023-12-14 10:30:30 +0700

    add filter by id for catalog

--- sales/filters/catalog.py
+++ sales/filters/catalog.py
@@ -1,12 +1,14 @@
 from django_filters import rest_framework as filters
 
+from sales.filters.estimate import FilterIDMixin
 from sales.models.catalog import Catalog, CatalogLevel
 
 
-class CatalogFilter(filters.FilterSet):
+class CatalogFilter(filters.FilterSet, FilterIDMixin):
     is_ancestor = filters.BooleanFilter('is_ancestor')
     level = filters.ModelChoiceFilter(queryset=CatalogLevel.objects.all())
+    id = filters.ModelMultipleChoiceFilter(queryset=CatalogLevel.objects.all(), method='get_by_id')
 
     class Meta:
         model = Catalog
-        fields = ('is_ancestor',)
+        fields = ('is_ancestor', 'id')
