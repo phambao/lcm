d595a637d51d95de576fa0376ac82328baea1ba9
Bao <bao.pham@fdssoft.com>
2023-12-14 10:57:28 +0700

    update queryset filter

--- sales/filters/catalog.py
+++ sales/filters/catalog.py
@@ -7,7 +7,7 @@ from sales.models.catalog import Catalog, CatalogLevel
 class CatalogFilter(filters.FilterSet, FilterIDMixin):
     is_ancestor = filters.BooleanFilter('is_ancestor')
     level = filters.ModelChoiceFilter(queryset=CatalogLevel.objects.all())
-    id = filters.ModelMultipleChoiceFilter(queryset=CatalogLevel.objects.all(), method='get_by_id')
+    id = filters.ModelMultipleChoiceFilter(queryset=Catalog.objects.all(), method='get_by_id')
 
     class Meta:
         model = Catalog
