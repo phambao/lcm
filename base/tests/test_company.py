65e2066f1c5d28e8d8e34a7e144de0fd66bca0d4
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2024-01-25 14:15:44 +0700

    test person information

--- base/tests/test_company.py
+++ base/tests/test_company.py
@@ -47,35 +47,37 @@ class CompanyTests(BaseTest):
             HTTP_AUTHORIZATION=self.token)
         self.assertEqual(res_data.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_update_company(self):
-    #     """Test delete company"""
-    #     data_company = {
-    #         "logo": "string",
-    #         "company_name": "company update",
-    #         "address": "string",
-    #         # "city": 1,
-    #         # "state": 1,
-    #         "zip_code": "string",
-    #         "tax": "string",
-    #         "size": 2,
-    #         "business_phone": "string",
-    #         "cell_phone": "string",
-    #         "fax": "string",
-    #         "email": "user@example.com",
-    #         "cell_mail": "string",
-    #         "user_create": self.user_id,
-    #         "user_update": self.user_id
-    #     }
-    #     res_delete = self.client.put(
-    #         f'/api/base/company/{self.company_id}/',
-    #         data_company,
-    #         format='json',
-    #         HTTP_AUTHORIZATION=self.token)
-    #     self.assertEqual(res_delete.status_code, status.HTTP_204_NO_CONTENT)
-    #
-    #     # Check if company is deleted
-    #     res_data = self.client.get(
-    #         f'/api/base/company/{self.company_id}/',
-    #         format='json',
-    #         HTTP_AUTHORIZATION=self.token)
-    #     self.assertEqual(res_data.status_code, status.HTTP_404_NOT_FOUND)
+    def test_create_person_information(self):
+        """Test create person information"""
+        data_person_information = {
+            "fullname": "string",
+            "phone_number": "string",
+            "email": "user@example.com",
+            "address": "string",
+            "company": self.company_id,
+            "first_name": "string",
+            "last_name": "string",
+            "nick_name": "string"
+        }
+        res_create = self.client.put(
+            f'/api/base/personal-information/',
+            data_person_information,
+            format='json',
+            HTTP_AUTHORIZATION=self.token)
+        self.person_information = res_create['id']
+        self.assertEqual(res_create.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_person_information(self):
+        """Test delete person information"""
+        res_delete = self.client.delete(
+            f'/api/base/personal-information/{self.person_information}/',
+            format='json',
+            HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(res_delete.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Check if company is deleted
+        res_data = self.client.get(
+            f'/api/base/personal-information/{self.person_information}/',
+            format='json',
+            HTTP_AUTHORIZATION=self.token)
+        self.assertEqual(res_data.status_code, status.HTTP_404_NOT_FOUND)
