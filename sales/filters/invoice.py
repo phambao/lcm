77339b3762783da45925ddb7bd2dcc3bf58d60d0
Bao <bao.pham@fdssoft.com>
2024-01-30 14:13:01 +0700

    add status, date_paid, deadline_datetime for invoice filter

--- sales/filters/invoice.py
+++ sales/filters/invoice.py
@@ -8,7 +8,11 @@ class InvoiceFilter(filters.FilterSet):
     created_date = filters.DateTimeFilter(field_name='created_date', lookup_expr='date')
     modified_date = filters.DateTimeFilter(field_name='modified_date', lookup_expr='date')
     proposal__lead = filters.ModelChoiceFilter(queryset=LeadDetail.objects.all())
+    status = filters.MultipleChoiceFilter(choices=Invoice.InvoiceStatus.choices)
+    date_paid = filters.DateTimeFilter(field_name='date_paid', lookup_expr='date')
+    deadline_datetime = filters.DateTimeFilter(field_name='deadline_datetime', lookup_expr='date')
 
     class Meta:
         model = Invoice
-        fields = ('name', 'created_date', 'modified_date', 'proposal__lead')
+        fields = ('name', 'created_date', 'modified_date', 'proposal__lead',
+                  'status', 'date_paid', 'deadline_datetime')
