7802fb01cae1d522da42d33733cf6ea1690e6277
Bao <bao.pham@fdssoft.com>
2024-03-31 23:19:18 +0700

    update status

--- sales/serializers/proposal.py
+++ sales/serializers/proposal.py
@@ -312,7 +312,7 @@ class ProposalWritingCompactSerializer(ContentTypeSerializerMixin):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['status'] = 'Draft'
+        data['status'] = instance.get_status()
         data['house_address'] = ''
         data['customer_contact'] = []
         if instance.lead:
