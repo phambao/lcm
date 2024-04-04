175f3703e80e6b7c224212a6c73957cac15bf6fe
NguyenXuanTruong53 <nguyenxuantruongee@gmail.com>
2024-04-04 15:58:41 +0700

    dealer page

--- lcm/settings.py
+++ lcm/settings.py
@@ -81,7 +81,6 @@ ROOT_URLCONF = 'lcm.urls'
 # }
 TEMPLATES = [
     {
-
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
