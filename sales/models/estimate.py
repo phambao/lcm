b0d72969baedf3524b15a615ccee7baaf9372697
Bao <bao.pham@fdssoft.com>
2024-04-23 17:14:05 +0700

    update tab for estimate template

--- sales/models/estimate.py
+++ sales/models/estimate.py
@@ -322,6 +322,7 @@ class EstimateTemplate(BaseModel):
                                default=list, blank=True, null=True)  # change order
     quantity = models.JSONField(blank=True, default=None, null=True)
     unit = models.ForeignKey('sales.UnitLibrary', on_delete=models.CASCADE, null=True, blank=True)
+    tab = models.IntegerField(default=0, blank=True, null=True)  # Change order
 
     def __str__(self):
         return self.name
