20ccd7136aebfe84f090c45f7478d554ff738618
Bao <bao.pham@fdssoft.com>
2024-05-17 10:21:30 +0700

    update filter by name for catalog

--- sales/filters/catalog.py
+++ sales/filters/catalog.py
@@ -8,7 +8,8 @@ class CatalogFilter(filters.FilterSet, FilterIDMixin):
     is_ancestor = filters.BooleanFilter('is_ancestor')
     level = filters.ModelChoiceFilter(queryset=CatalogLevel.objects.all())
     id = filters.ModelMultipleChoiceFilter(queryset=Catalog.objects.all(), method='get_by_id')
+    name = filters.CharFilter('name', lookup_expr='icontains')
 
     class Meta:
         model = Catalog
-        fields = ('is_ancestor', 'id')
+        fields = ('is_ancestor', 'id', 'name')
