807791bed75dd5bfe7edc1c0794bdf377f774b30
Truong <truong.nguyen@fdssoft.com>
2024-06-04 11:31:52 +0700

    update api jobs

--- sales/serializers/lead_list.py
+++ sales/serializers/lead_list.py
@@ -479,3 +479,9 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = lead_list.Job
         fields = '__all__'
+        extra_kwargs = {**extra_kwargs_for_base_model(), **{'company': {'read_only': True},
+                                                            'user_update': {'read_only': True},
+                                                            'user_create': {'read_only': True},
+                                                            'modified_date': {'read_only': True},
+                                                            'created_date': {'read_only': True}
+                                                            }}
